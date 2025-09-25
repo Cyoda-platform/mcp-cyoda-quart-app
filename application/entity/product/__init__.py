6d9d9a1fd32ac610e6f8a1bd0655bb2ddf06f6a8
app-builder <app-builder@example.com>
2025-09-25 14:26:47 +0000

    Generated code using Auggie CLI - tasks completed

---
+++ application/entity/product/__init__.py
@@ -0,0 +1,9 @@
+# application/entity/product/__init__.py
+
+"""
+Product entity package for Pet Store Performance Analysis System.
+"""
+
+from .version_1.product import Product
+
+__all__ = ["Product"]
